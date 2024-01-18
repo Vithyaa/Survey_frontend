a8471f5c9976bde4609b6878233408cc90b732cb
Jayan Keasav Vennih <jayan@agilecybersolutions.com>
2024-01-18 11:32:17 +0530

    Routing set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SurveyPageComponent } from './pages/survey-page/survey-page.component';
+import { ThankPageComponent } from './pages/thank-page/thank-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'survey', component: SurveyPageComponent },
+  { path: 'thankyou', component: ThankPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
